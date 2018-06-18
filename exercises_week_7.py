6e0b519c471d8a86f34fa15585679c06a807fa08
Floor Harmsen <floor.harmsen@gmail.com>
2018-06-18 18:31:22 +0200

    week 7 day 2 lesson using dictionaries

--- exercises_week_7.py
+++ exercises_week_7.py
@@ -63,3 +63,85 @@ plays["Respect"] = 94
 library = {"The Best Songs": plays, "Sunday Feelings": {}}
 
 print(library)
+
+# --------------------- Lesson: Using Dictionaries --------------------- #
+
+# 1. Using Dictionaries
+
+# 2. Get a Key
+zodiac_elements = {"water": ["Cancer", "Scorpio", "Pisces"], "fire": ["Aries", "Leo", "Sagittarius"], "earth": ["Taurus", "Virgo", "Capricorn"], "air":["Gemini", "Libra", "Aquarius"]}
+
+print(zodiac_elements["earth"])
+print(zodiac_elements["fire"])
+
+# 3. Get an invalid key
+zodiac_elements["energy"] = "Not a Zodiac element"
+
+print(zodiac_elements["energy"])
+
+# 4. Try/Exept to get a key
+caffeine_level = {"espresso": 64, "chai": 40, "decaf": 0, "drip": 120}
+
+caffeine_level['matcha'] = 30
+
+try:
+  print(caffeine_level['matcha'])
+except KeyError:
+  print("Unknown Caffeine Level")
+
+# 5. Safely get a key
+user_ids = {"teraCoder": 100019, "pythonGuy": 182921, "samTheJavaMaam": 123112, "lyleLoop": 102931, "keysmithKeith": 129384}
+
+tc_id = user_ids.get("teraCoder", 100000)
+print(tc_id)
+
+stack_id = user_ids.get("superStackSmash", 100000)
+print(stack_id)
+
+# 6. Delete a key
+available_items = {"health potion": 10, "cake of the cure": 5, "green elixir": 20, "strength sandwich": 25, "stamina grains": 15, "power stew": 30}
+health_points = 20
+
+health_points += available_items.pop("stamina grains", 0)
+health_points += available_items.pop("power stew", 0)
+health_points += available_items.pop("mystic bread", 0)
+
+print(available_items, health_points)
+
+# 7. Get all keys
+user_ids = {"teraCoder": 100019, "pythonGuy": 182921, "samTheJavaMaam": 123112, "lyleLoop": 102931, "keysmithKeith": 129384}
+num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
+
+users = user_ids.keys()
+lessons = num_exercises.keys()
+
+print(users)
+print(lessons)
+
+# 8. Get all values
+num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
+
+total_exercises = 0
+
+for exercise in num_exercises.values():
+  total_exercises += exercise
+
+print(total_exercises)
+
+# 9. Get all items
+pct_women_in_occupation = {"CEO": 28, "Engineering Manager": 9, "Pharmacist": 58, "Physician": 40, "Lawyer": 37, "Aerospace Engineer": 9}
+
+for function, value in pct_women_in_occupation.items():
+  print("Women make up " + str(value) + " percent of " + str(function) + "s.")
+
+# 10. Review
+tarot = { 1:	"The Magician", 2:	"The High Priestess", 3:	"The Empress", 4:	"The Emperor", 5:	"The Hierophant", 6:	"The Lovers", 7:	"The Chariot", 8:	"Strength", 9:	"The Hermit", 10:	"Wheel of Fortune", 11:	"Justice", 12:	"The Hanged Man", 13:	"Death", 14:	"Temperance", 15:	"The Devil", 16:	"The Tower", 17:	"The Star", 18:	"The Moon", 19:	"The Sun", 20:	"Judgement", 21:	"The World", 22: "The Fool"}
+
+spread = {}
+
+spread["past"] = tarot.pop(13)
+spread["present"] = tarot.pop(22)
+spread["future"] = tarot.pop(10)
+
+for key, value in spread.items():
+  print("Your " + str(key) + " is the " + str(value) + " card.")
