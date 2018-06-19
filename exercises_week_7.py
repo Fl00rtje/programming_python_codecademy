c955709511c2d9f20c7eb6ea00763d5d89a6b7a8
Floor Harmsen <floor.harmsen@gmail.com>
2018-06-19 21:46:06 +0200

    week 7 day 4 - Dictionaries - Code Challenge

--- exercises_week_7.py
+++ exercises_week_7.py
@@ -183,3 +183,129 @@ for player, words in player_to_words.items():
     player_to_points[player] = player_points
 
 print(player_to_points)
+
+# --------------------- Lesson: Dictionaries - Code Challenge --------------------- #
+
+# 1. Introduction
+
+# 2. Sum Values
+def sum_values(my_dictionary):
+  sum = 0
+  for value in my_dictionary.values():
+    sum += value
+  return sum
+
+print(sum_values({"milk":5, "eggs":2, "flour": 3}))
+# should print 10
+print(sum_values({10:1, 100:2, 1000:3}))
+# should print 6
+
+# 3. Even keys
+def sum_even_keys(my_dictionary):
+    sum = 0
+    for key, value in my_dictionary.items():
+        if key % 2 == 0:
+            sum += value
+    return sum
+
+print(sum_even_keys({1: 5, 2: 2, 3: 3}))
+# should print 2
+print(sum_even_keys({10: 1, 100: 2, 1000: 3}))
+# should print 6
+
+# 4. Add ten
+def add_ten(my_dictionary):
+  for key, value in my_dictionary.items():
+    my_dictionary[key] = value + 10
+  return my_dictionary
+
+print(add_ten({1:5, 2:2, 3:3}))
+# should print {1:15, 2:12, 3:13}
+print(add_ten({10:1, 100:2, 1000:3}))
+# should print {10:11, 100:12, 1000:13}
+
+# 5. Values that are keys
+def values_that_are_keys(my_dictionary):
+  values_keys = []
+  for key in my_dictionary.keys():
+    for value in my_dictionary.values():
+      if key == value:
+        values_keys.append(value)
+  return values_keys
+
+print(values_that_are_keys({1:100, 2:1, 3:4, 4:10}))
+# should print [1, 4]
+print(values_that_are_keys({"a":"apple", "b":"a", "c":100}))
+# should print ["a"]
+
+# 6. Largest value
+def max_key(my_dictionary):
+  max_value = 0
+  max_key = 0
+  for key, value in my_dictionary.items():
+    if value > max_value:
+      max_value = value
+      max_key = key
+  return max_key
+
+print(max_key({1:100, 2:1, 3:4, 4:10}))
+# should print 1
+print(max_key({"a":100, "b":10, "c":1000}))
+# should print "c"
+print(max_key({"Floor": 100, "Jaap": 75, "Pieter": 87}))
+
+# 7. Word length dict
+def word_length_dictionary(words):
+  word_length = {}
+  for word in words:
+    word_length[word] = len(word)
+  return word_length
+
+print(word_length_dictionary(["apple", "dog", "cat"]))
+# should print {"apple":5, "dog": 3, "cat":3}
+print(word_length_dictionary(["a", ""]))
+# should print {"a": 1, "": 0}
+
+# 8. Frequency count
+def frequency_dictionary(words):
+  word_frequency = {}
+  for word in words:
+    word_frequency[word] = words.count(word)
+  return word_frequency
+
+print(frequency_dictionary(["apple", "apple", "cat", 1]))
+# should print {"apple":2, "cat":1, 1:1}
+print(frequency_dictionary([0,0,0,0,0]))
+# should print {0:5}
+
+# 9. Unique values
+def unique_values(my_dictionary):
+  seen_values = []
+  for value in my_dictionary.values():
+    if value in seen_values:
+      continue
+    else:
+      seen_values.append(value)
+  return len(seen_values)
+
+print(unique_values({0:3, 1:1, 4:1, 5:3}))
+# should print 2
+print(unique_values({0:3, 1:3, 4:3, 5:3}))
+# should print 1
+
+# 10. Count first letter
+def count_first_letter(names):
+  letter_people = {}
+  for key, value in names.items():
+    first_letter = key[0]
+    if first_letter in letter_people:
+      letter_people[first_letter] += len(value)
+    else:
+      letter_people[key[0]] = len(value)
+  return letter_people
+
+print(count_first_letter({"Stark": ["Ned", "Robb", "Sansa"], "Snow" : ["Jon"], "Lannister": ["Jaime", "Cersei", "Tywin"]}))
+# should print {"S": 4, "L": 3}
+print(count_first_letter({"Stark": ["Ned", "Robb", "Sansa"], "Snow" : ["Jon"], "Sannister": ["Jaime", "Cersei", "Tywin"]}))
+# should print {"S": 7}
+
