d7f03b2b92f0b7c2dc78d5aa7d8f3e47f0512932
Floor Harmsen <floor.harmsen@gmail.com>
2018-06-19 16:43:02 +0200

    week 7 day 3 - project: scrabble

--- exercises_week_7.py
+++ exercises_week_7.py
@@ -145,3 +145,41 @@ spread["future"] = tarot.pop(10)
 
 for key, value in spread.items():
   print("Your " + str(key) + " is the " + str(value) + " card.")
+
+# --------------------- Project: Scrabble --------------------- #
+
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
+           "W", "X", "Y", "Z"]
+points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
+
+zipped_letters = zip(letters, points)
+letter_to_points = {key: value for key, value in zipped_letters}
+letter_to_points[" "] = 0
+
+print(letter_to_points)
+
+def score_word(word):
+    point_total = 0
+    for letter in word:
+        if letter in word:
+            point_total += letter_to_points[letter]
+        else:
+            point_total += 0
+    return point_total
+
+
+brownie_points = score_word("BROWNIE")
+print(brownie_points)
+
+player_to_words = {"player1": ["BLUE", "TENNIS", "EXIT"], "wordNerd": ["EARTH", "EYES", "MACHINE"],
+                   "Lexi Con": ["ERASER", "BELLY", "HUSKY"], "Prof Reader": ["ZAP", "COMA", "PERIOD"]}
+
+player_to_points = {}
+
+for player, words in player_to_words.items():
+    player_points = 0
+    for word in words:
+        player_points += score_word(word)
+    player_to_points[player] = player_points
+
+print(player_to_points)
